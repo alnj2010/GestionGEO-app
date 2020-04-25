9e8f159608ef723a043a0ebc578b8579c8747997
Abraham Navarro <abrahancho6@gmail.com>
2020-04-25 10:12:39 -0400

    home admin

--- src/components/Home/admin.js
+++ src/components/Home/admin.js
@@ -5,6 +5,9 @@ import Typography from '@material-ui/core/Typography';
 
 import { getSessionUser } from '../../storage/sessionStorage';
 
+import imgMan from '../../images/hombre.png';
+import imgWomen from '../../images/mujer.png';
+
 const styles = () => ({
     welcome: {
         backgroundColor: '#FAFAFA',
@@ -12,12 +15,22 @@ const styles = () => ({
         borderRadius: '5px',
         boxShadow: '-1px 2px 5px 0px rgba(0,0,0,0.2)',
     },
-    welcome__picture: {
+    welcome__pictureContainer: {
         width: '146px',
-        backgroundColor: 'black',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    welcome__picture: {
+        width: '100%',
+        minWidth: '77px',
+        height: 'auto',
     },
     welcome__info: {
         padding: '10px',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
     },
 });
 class AdminHome extends Component {
@@ -29,14 +42,19 @@ class AdminHome extends Component {
             second_name,
             first_surname,
             second_surname,
+            sex,
             administrator: { rol, principal },
         } = getSessionUser();
 
         return (
             <Grid container className={classes.welcome}>
                 <Grid item container xs={2} justify="center">
-                    <Grid item className={classes.welcome__picture}>
-                        foto
+                    <Grid item className={classes.welcome__pictureContainer}>
+                        <img
+                            src={sex === 'M' ? imgMan : imgWomen}
+                            alt="admin"
+                            className={classes.welcome__picture}
+                        />
                     </Grid>
                 </Grid>
                 <Grid item xs={9} className={classes.welcome__info}>
