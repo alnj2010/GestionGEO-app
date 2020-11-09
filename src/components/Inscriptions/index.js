b1d3c7077d654575fa2d9ba134ddcde3409cecaf
Abraham Navarro <anavarro@teravisiontech.com>
2020-11-09 14:16:41 -0400

    adding financing

--- src/components/Inscriptions/index.js
+++ src/components/Inscriptions/index.js
@@ -1,33 +1,56 @@
 import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import MaterialTable, { MTableToolbar } from 'material-table';
-import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
+import { Form, reduxForm } from 'redux-form';
+import MaterialTable from 'material-table';
+import { withStyles } from '@material-ui/core/styles';
 
-import { Grid, Fab } from '@material-ui/core';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
+import { Grid, Fab, Typography } from '@material-ui/core';
+import RenderFields from '../RenderFields';
+import CustomizedSnackbar from '../Snackbar';
+import { FINANCING_TYPE } from '../../services/constants';
+import { jsonToOptions } from '../../helpers';
 import Table from './Table';
 import TableBodyRow from './Row';
 
-function Inscription({ subjects, saveInscription, width, finalWorks }) {
+const styles = () => ({
+  tableContainer: {
+    marginTop: 20,
+  },
+});
+function Inscription({
+  subjects,
+  saveInscription,
+  width,
+  finalWorks,
+  classes,
+  handleSubmit,
+  pristine,
+  submitting,
+  valid,
+}) {
   const matches = isWidthUp('sm', width);
 
   const [finalWorksData, setFinalWorksData] = useState([]);
   const tableFinalWorkRef = useRef(null);
   const tableSubjectRef = useRef(null);
   useEffect(() => {
-    setFinalWorksData(
-      finalWorks.map((fw) => {
-        return {
-          id: fw.id,
-          code: fw.code,
-          name: fw.name,
-          laboratoryHours: fw.laboratory_hours,
-          practicalHours: fw.practical_hours,
-          theoreticalHours: fw.theoretical_hours,
-          uc: fw.uc,
-          title: 'Sin titulo',
-        };
-      })
-    );
+    if (finalWorks && finalWorks.length) {
+      setFinalWorksData(
+        finalWorks.map((fw) => {
+          return {
+            id: fw.id,
+            code: fw.code,
+            name: fw.name,
+            laboratoryHours: fw.laboratory_hours,
+            practicalHours: fw.practical_hours,
+            theoreticalHours: fw.theoretical_hours,
+            uc: fw.uc,
+            title: 'Sin titulo',
+          };
+        })
+      );
+    }
   }, [finalWorks]);
 
   const transformData = () => {
@@ -45,12 +68,41 @@ function Inscription({ subjects, saveInscription, width, finalWorks }) {
     return [];
   };
 
+  let isFinalSubject;
+  if (finalWorks && finalWorks.length) {
+    const [item] = finalWorks;
+    isFinalSubject = item.is_final_subject;
+  }
+  const handleInscriptionStudent = (values) => {
+    const enrolledSubjects = tableSubjectRef.current.props.data.filter(
+      (item) => item.tableData.checked
+    );
+    let enrolledProjectOrWorks;
+    let enrolledFinalWorks;
+    let enrolledProjects;
+
+    if (tableFinalWorkRef) {
+      enrolledProjectOrWorks = tableFinalWorkRef.current.props.data.filter(
+        (item) => item.tableData.checked
+      );
+
+      if (isFinalSubject) {
+        enrolledFinalWorks = enrolledProjectOrWorks;
+      } else {
+        enrolledProjects = enrolledProjectOrWorks;
+      }
+    }
+    saveInscription({ enrolledSubjects, enrolledProjects, enrolledFinalWorks, ...values });
+  };
   return (
     <Grid container>
+      <Typography variant="h6" gutterBottom>
+        Materias disponibles para inscripción
+      </Typography>
       <Grid item xs={12}>
         <MaterialTable
           tableRef={tableSubjectRef}
-          title={matches ? 'Inscripcion' : ''}
+          title={matches ? 'inscripción' : ''}
           components={{
             Toolbar: () => null,
           }}
@@ -68,8 +120,8 @@ function Inscription({ subjects, saveInscription, width, finalWorks }) {
           ]}
           data={transformData()}
           localization={{
-            header: {
-              actions: 'Acciones',
+            body: {
+              emptyDataSourceMessage: 'No hay materias disponibles',
             },
           }}
           options={{
@@ -80,73 +132,110 @@ function Inscription({ subjects, saveInscription, width, finalWorks }) {
           }}
         />
       </Grid>
-      <Grid item xs={12}>
-        <Table
-          tableRef={tableFinalWorkRef}
-          title={matches ? 'Inscripcion' : ''}
-          options={{
-            selection: true,
-            search: false,
-            paging: false,
-            actionsColumnIndex: -1,
-          }}
-          components={{
-            Row: TableBodyRow,
-            Toolbar: () => null,
-          }}
-          columns={[
-            {
-              title: 'id',
-              field: 'id',
-              hidden: true,
-            },
-            {
-              title: 'Codigo',
-              field: 'code',
-              editable: 'never',
-            },
-            {
-              title: 'Titulo',
-              field: 'title',
-              editable: 'onUpdate',
-            },
-            { title: 'Nombre', field: 'name', editable: 'never' },
-            { title: 'Unidades de Credito', field: 'uc', editable: 'never' },
-          ]}
-          data={finalWorksData}
-          editable={{
-            onRowUpdate: (newData, oldData) =>
-              new Promise((resolve, reject) => {
-                const data = [...finalWorksData];
-                const index = data.indexOf(oldData);
-                data[index] = newData;
-                setFinalWorksData(data);
+      {finalWorks && finalWorks.length ? (
+        <Grid item className={classes.tableContainer} xs={12}>
+          <Table
+            tableRef={tableFinalWorkRef}
+            title={matches ? 'inscripción' : ''}
+            options={{
+              selection: true,
+              search: false,
+              paging: false,
+              actionsColumnIndex: -1,
+            }}
+            components={{
+              Row: TableBodyRow,
+              Toolbar: () => null,
+            }}
+            columns={[
+              {
+                title: 'id',
+                field: 'id',
+                hidden: true,
+              },
+              {
+                title: 'Codigo',
+                field: 'code',
+                editable: 'never',
+              },
+              {
+                title: 'Titulo',
+                field: 'title',
+                editable: 'onUpdate',
+              },
+              { title: 'Nombre', field: 'name', editable: 'never' },
+              { title: 'Unidades de Credito', field: 'uc', editable: 'never' },
+            ]}
+            data={finalWorksData}
+            localization={{
+              body: {
+                emptyDataSourceMessage: 'No hay Trabajos finales disponibles',
+              },
+            }}
+            editable={{
+              onRowUpdate: (newData, oldData) =>
+                new Promise((resolve, reject) => {
+                  const data = [...finalWorksData];
+                  const index = data.indexOf(oldData);
+                  data[index] = newData;
+                  setFinalWorksData(data);
 
-                resolve();
-              }),
-          }}
-        />
-      </Grid>
+                  resolve();
+                }),
+            }}
+          />
+        </Grid>
+      ) : null}
+      <Grid container className={classes.tableContainer}>
+        <Form onSubmit={handleSubmit(handleInscriptionStudent)} style={{ width: '100%' }}>
+          <Grid container>
+            <Grid item xs={3}>
+              <RenderFields>
+                {[
+                  {
+                    field: 'financing',
+                    id: 'financing',
+                    type: 'select',
+                    label: 'financiación',
+                    options: jsonToOptions(FINANCING_TYPE),
+                  },
+                ]}
+              </RenderFields>
+            </Grid>
+            <Grid item xs={6}>
+              <RenderFields>
+                {[
+                  {
+                    label: 'Descripcion financiación',
+                    field: 'financing_description',
+                    id: 'financing_description',
+                    type: 'text',
+                  },
+                ]}
+              </RenderFields>
+            </Grid>
 
-      <Grid container justify="center">
-        <Fab
-          variant="extended"
-          size="medium"
-          color="primary"
-          aria-label="Add"
-          onClick={() => {
-            const enrolledSubjects = tableSubjectRef.current.props.data.filter(
-              (item) => item.tableData.checked
-            );
-            const enrolledFinalWorks = tableSubjectRef.current.props.data.filter(
-              (item) => item.tableData.checked
-            );
-            console.log({ enrolledSubjects, enrolledFinalWorks });
-            //saveInscription({ enrolledSubjects, enrolledFinalWorks });
-          }}
-        >
-          Inscribir
-        </Fab>
+            <Grid item container justify="center" xs={3}>
+              <Fab
+                variant="extended"
+                size="medium"
+                color="primary"
+                aria-label="Add"
+                disabled={
+                  !valid ||
+                  pristine ||
+                  submitting ||
+                  !tableSubjectRef.current.props.data.filter((item) => item.tableData.checked)
+                    .length
+                }
+                type="submit"
+              >
+                Inscribir
+              </Fab>
+            </Grid>
+          </Grid>
+          <CustomizedSnackbar />
+        </Form>
       </Grid>
     </Grid>
   );
@@ -158,4 +247,20 @@ Inscription.propTypes = {
   width: PropTypes.string.isRequired,
 };
 
-export default withWidth()(Inscription);
+const inscriptionStudentValidator = (values) => {
+  const errors = {};
+  if (!values.financing_description) {
+    errors.financing_description = 'descripcion requerida';
+  }
+  if (!values.financing) {
+    errors.financing = 'financiacion requerida';
+  }
+  return errors;
+};
+
+const InscriptionWrapper = reduxForm({
+  form: 'inscriptionStudent',
+  validate: inscriptionStudentValidator,
+})(Inscription);
+
+export default withStyles(styles)(withWidth()(InscriptionWrapper));
--- src/containers/Inscriptions/index.js
+++ src/containers/Inscriptions/index.js
@@ -7,7 +7,7 @@ import {
   inscription,
   cleanAvailableSubjects,
 } from '../../actions/studentInscription';
-import { getSessionStudentId } from '../../storage/sessionStorage';
+import { getSessionStudentId, getSessionUser } from '../../storage/sessionStorage';
 
 class InscriptionContainer extends Component {
   componentDidMount = () => {
@@ -21,14 +21,43 @@ class InscriptionContainer extends Component {
     cleanAvailableSubjectsDispatch();
   };
 
-  saveInscription = (value) => {
+  saveInscription = ({
+    enrolledSubjects,
+    enrolledFinalWorks,
+    enrolledProjects,
+    financing,
+    financing_description: financingDescription,
+  }) => {
     const { inscriptionDispatch, history } = this.props;
+    const {
+      student: { id, current_status: currentStatus },
+    } = getSessionUser();
     const payload = {
-      student_id: getSessionStudentId(),
-      status: 'REG',
-      subjects: value.map((subject) => ({
+      student_id: id,
+      status: currentStatus,
+      financing,
+      financing_description: financingDescription,
+      subjects: enrolledSubjects.map((subject) => ({
         school_period_subject_teacher_id: subject.school_period_subject_teacher_id,
       })),
+      final_works:
+        enrolledFinalWorks && enrolledFinalWorks.length
+          ? enrolledFinalWorks.map((item) => ({
+              title: item.title,
+              subject_id: 13,
+              project_id: 4,
+              advisors: item.advisors.map((advisor) => ({
+                teacher_id: advisor.id,
+              })),
+            }))
+          : undefined,
+      projects:
+        enrolledProjects && enrolledProjects.length
+          ? enrolledProjects.map((item) => ({
+              title: item.title,
+              subject_id: item.id,
+            }))
+          : undefined,
     };
     inscriptionDispatch(payload).then((res) => {
       if (res) {
