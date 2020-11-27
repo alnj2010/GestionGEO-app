716f76a27a57fe78e5c7085e7aa9f7149f761462
alhec <hector080896@gmail.com>
2020-11-26 20:40:19 -0400

    docker

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -22,6 +22,7 @@ export function headers(type) {
   }
   return items;
 }
+
 const AXIOS = axios.create({
   baseURL: window._env_.REACT_APP_API_URL,
   headers: {
--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
-      <p>API_URL: {window._env_.REACT_APP_API_URL}</p>
-      <App />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
